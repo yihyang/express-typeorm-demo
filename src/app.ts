772f93ab08a2bfcf7cba2d5449765a7ebcaeaa67
Yih Yang <yyngo788n88@hotmail.com>
2019-03-05 23:09:50 +0800

    added express logic

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-console.log('Application is up and running')
+import * as express from "express";
+import {Request, Response} from "express";
+import * as bodyParser from  "body-parser";
+
+// create and setup express app
+const app = express();
+app.use(bodyParser.json());
+
+// register routes
+
+app.get("/users", function(req: Request, res: Response) {
+    // here we will have logic to return all users
+});
+
+app.get("/users/:id", function(req: Request, res: Response) {
+    // here we will have logic to return user by id
+});
+
+app.post("/users", function(req: Request, res: Response) {
+    // here we will have logic to save a user
+});
+
+app.put("/users/:id", function(req: Request, res: Response) {
+    // here we will have logic to update a user by a given user id
+});
+
+app.delete("/users/:id", function(req: Request, res: Response) {
+    // here we will have logic to delete a user by a given user id
+});
+
+// start express server
+app.listen(3000);
